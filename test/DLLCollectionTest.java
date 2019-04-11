07db99f66cb46b4609781826e09bd9e65b9df454
ranefer <sul.lyon.130@gmail.com>
2019-04-11 09:24:56 -0600

    remove .class files, delete default test

--- test/DLLCollectionTest.java
+++ test/DLLCollectionTest.java
@@ -1,16 +1,7 @@
-import static org.junit.Assert.fail;
-
-import org.junit.Test;
-
 import collection.Collection;
 import collection.DoublyLinkedList.DoublyLinkedList;
 
 public class DLLCollectionTest extends ACollectionTest {
-
-	@Test
-	public void test() {
-		fail("Not yet implemented");
-	}
 
 	@Override
 	protected Collection<String> setCollection() {
